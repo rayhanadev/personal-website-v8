63fb44c1977d9b832195b21ded1c00963110c565
Rayhan Noufal Arayilakath <me@rayhanadev.com>
2025-12-11 01:49:29 -0500

    feat: update tooling

--- .prettierrc.js
+++
@@ -1,5 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-    tabWidth: 4,
-    plugins: ["prettier-plugin-astro"],
-};
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Dev shell with Node 22 and Bun (latest)";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bun
+            pkgs.nodejs_22
+          ];
+        };
+      });
+}
